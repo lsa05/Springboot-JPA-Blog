26ce1d0b1ce4f02f7c729561c502e2446a2b9873
lsa05 <ssangah05@gmail.com>
2023-03-06 23:06:35 +0900

    테스트

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -15,7 +15,8 @@ let index = {
 			
 			//console.log(data);
 			
-			$.ajax().done().fail();//ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!						
+			$.ajax().done().fail();//ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!	
+								
 		}
 }
 
